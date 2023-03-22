ca195da11fe789c8bcc3e1a704a1908e05e6d7e5
jagriti mishra <jagritimishra1110@gmail.com>
2023-03-23 02:42:44 +0530

    Massafe

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sdata from './Sdata'
+// import img1 from '..//../public/assest/trai1.avif'
 
 function Card(props) {
   return (
     <>
     <div className='chats'>
+      {/* <img src={img1} className='h-[20vh]' /> */}
     <div className='userChat p-1.5 sm:flex items-center gap-2 text-white cursor-pointer hover:bg-sky-800/60 '>
           <img className='h-[50px] rounded-full object-cover w-[50px]' src={props.imgsrc} altimgsrc />
           <div className='userchatinfo'>
